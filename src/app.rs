dd396ef1a1873bed62099a07b0e3cc3a6c3b7607
Ross Curley <ross.curley@outlook.com>
2026-05-23 16:01:39 +0800

    started app.rs

---
+++ src/app.rs
@@ -0,0 +1,36 @@
+use std::path::PathBuf;
+
+pub struct AppConfig {
+    pub safe_mode: bool,
+    pub tick_rate: u64,
+    pub log_tail_lines: u64,
+    pub hosts: Vec<HostConfig>,
+}
+
+pub struct HostConfig {
+    pub name: String,
+    pub connection: ConnetionType,
+}
+
+pub struct ConnectionType {
+    pub host: String,
+    pub port: u16,
+    pub tls: Option<TlsConfig>,
+}
+
+pub struct TlsConfig {
+    pub cert_path: PathBuf,
+    pub ca_cert: Option<PathBuf>,
+    pub verify: bool,
+}
+
+impl AppConfig {
+    pub fn new(safe_mode: bool, tick_rate: u64, log_tail_lines: u64, hosts: Vec<HostConfig>) -> Self {
+        Self {
+            safe_mode,
+            tick_rate,
+            log_tail_lines,
+            hosts,
+        }
+    }
+}
