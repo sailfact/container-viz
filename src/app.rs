8fafbe9a4aad33a379faac7ca793e1539602dae1
Ross Curley <ross.curley@outlook.com>
2026-05-25 09:35:25 +0800

    added impl AppState

--- src/app.rs
+++ src/app.rs
@@ -30,3 +30,20 @@ pub struct HostState {
     pub selected: usize,
     pub composed_groups: HashMap<String, Vec<ContainerInfo>>,
 }
+
+impl AppState {
+    pub fn new(&self, config: AppConfig) -> Self {}
+    pub fn active_host(&self) &HostState {}
+    pub fn active_host_mut(&self) &mut HostState {}
+    pub fn next_tab(&mut self) {}
+    pub fn prev_tab(&mut self) {}
+    pub fn apply_update(host_idx: usize, update: HostUpdate) {}
+    pub fn dispatch_command(host_idx: usize, cmd: HostCommand) {}
+    pub fn set_mode(mode: AppMode) {}
+    pub fn toggle_safe_mode() {}
+    pub fn toggle_detail() {}
+    pub fn set_pending_action(action: PendingAction) {}
+    pub fn clear_pending_action() {}
+    pub fn set_status(msg: String, level: MessageLevel) {}
+    pub fn tick_status() {}
+}
